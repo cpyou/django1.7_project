9bee85b69fec3cd7b0ceb333deeb65dc0275d85a
puyu.chen <745690939@qq.com>
2015-01-08 12:33:46 +0800

    [Add] listplay

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 from polls.models import Choice, Question
 
-class ChoiceInline(admin.StackedInline):
+# display list use 'TabularInline',general display use 'StackedInline'
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     # fields = ['pub_date', 'question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
     inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
